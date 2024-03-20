8c0e7759c67f56a4b10c69e61341ed7dae159d09
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-03-20 22:28:45 +0530

    "appwrite client"

---
+++ 13_appwriteReact/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  console.log('your value : ',import.meta.env.VITE_APPRITE_URL);
+  return (
+    <>
+      <h1>Blog app with Appwrite</h1>
+    </>
+  )
+}
+
+export default App
