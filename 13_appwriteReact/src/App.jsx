d882ec74d6786044593f2dbdec7d6aa14c2401cf
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-03-21 02:49:34 +0530

    redux

--- 13_appwriteReact/src/App.jsx
+++ 13_appwriteReact/src/App.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from '/src/components/index.js';
 import './App.css'
 
 function App() {
-  console.log('your value : ',import.meta.env.VITE_APPRITE_URL);
-  return (
-    <>
-      <h1>Blog app with Appwrite</h1>
-    </>
-  )
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO : {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
+
 }
 
 export default App
