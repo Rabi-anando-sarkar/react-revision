6759138f2ecbc13d6a35000ecbfab7861a959ce2
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-03-22 18:22:35 +0530

    components continued

--- 13_appwriteReact/src/components/index.js
+++ 13_appwriteReact/src/components/index.js
@@ -3,11 +3,23 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Select from "./Select"
+import Button from "./Button"
+import Input from "./Input"
+import AuthLayout from "./AuthLayout"
 
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Login,
+    PostCard,
+    Select,
+    Button,
+    Input,
+    AuthLayout,
 }
